f616fbefa430ab93141c35e88260132b3223f930
Chumak Vladimir <parsival427@gmail.com>
2024-02-26 10:34:14 +0300

    clear project

--- main.py
+++ main.py
@@ -2,22 +2,3 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.write("Test")
-components.html("""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="manifest" href="/manifest.json">
-    <script>
-        if('serviceWorker' in navigator) {
-          navigator.serviceWorker.register('/sw.js', { scope: '/' });
-        }
-    </script>
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-""")
